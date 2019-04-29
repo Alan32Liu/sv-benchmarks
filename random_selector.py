600f74bb2c72a80380d625cea37e7e9e19d2db33
DonggeLiu <alan32.liu@gmail.com>
2019-04-29 15:22:24 +1000

    a script to randomly select a number of benchmarks, execute legion on them, and report coverage

--- random_selector.py
+++ random_selector.py
@@ -1,6 +1,5 @@
 """Randomly selects the file to instrument and test with."""
 
-import csv
 import json
 import os
 import pdb
@@ -8,23 +7,37 @@ import random
 import re
 import threading
 import subprocess
-from matplotlib import pyplot
 
 
 NUM_FILES = 0
-NUM_NEEDS = 4
+NUM_NEEDS = 1
+BLACKLIST = 'BlacklistBenchmarks'
 LENGTH_DICT_FILE = 'length_dict_i'
 SLOC_MEASURE = "grep 'Total Physical Source Lines of Code (SLOC)' | grep -o '[0-9,]*'"
 GCOV_CMD = "gcc -fprofile-arcs -ftest-coverage -o "
-LEGION_CMD = "python3 principes.py ../Benchmarks/sv-benchmarks/c/instr/"
 LEGION_DIR = "../../Principes"
-LEGION_RUNTIME = 60
 INPUTS_DIR = "{}/inputs".format(LEGION_DIR)
+
+TIME_RESTRICTION = int(1/6 * 60 * 60)   # seconds
+MEMO_RESTRICTION = 64 * 1000 * 1000  # KB
+RESTRICTED_LEGION = 'restricted_legion.sh'
+RESTRICTION = "ulimit -t {} -m {} -v {}\n".format(
+    TIME_RESTRICTION, MEMO_RESTRICTION, MEMO_RESTRICTION)
+print(RESTRICTION)
+LEGION_CMD_PREFIX = "python3 principes.py ../Benchmarks/sv-benchmarks/c/instr"
+# pdb.set_trace()
 
 
 def parse_target_dirs():
     with open('target_dirs', 'r') as file_targets:
         return [target[:-7] for target in file_targets.readlines()]
+
+
+def parse_blacklist():
+    with open(BLACKLIST, 'r') as blacklist_file:
+        # .instr\n are the last six chars,
+        # we want to leave it as .i
+        return [target[:-5] for target in blacklist_file.readlines()]
 
 
 def get_length_dict():
@@ -45,15 +58,15 @@ def read_length_dict():
 
 
 def generate_length_dict():
-    target_is, length_dict = [], {}
+    target_is, tmp_length_dict = [], {}
     for target_dir in TARGET_DIRS:
         for file in os.listdir("c/{}".format(target_dir)):
-            process_file(target_dir, file, length_dict)
+            process_file(target_dir, file, tmp_length_dict)
 
     with open(LENGTH_DICT_FILE, 'w') as f:
-        json.dump(length_dict, f)
+        json.dump(tmp_length_dict, f)
 
-    return length_dict
+    return tmp_length_dict
 
 
 def process_file(target_dir, file, length_dict):
@@ -95,25 +108,62 @@ def record_file(target_i, num_line, length_dict):
     NUM_FILES += 1
 
 
-def execute_legion(code):
-    def on_timeout(proc, status_dict):
-        """Kill process on timeout and note as status_dict['timeout']=True"""
-        # a container used to pass status back to calling thread
-        status_dict['timeout'] = True
-        print("timed out")
-        proc.kill()
-    status_dict = {'timeout': False}
-    command = "{}{} 0".format(LEGION_CMD, code).split(" ")
-    proc = subprocess.Popen(args=command, cwd=LEGION_DIR)
-    timer = threading.Timer(LEGION_RUNTIME, on_timeout, (proc, status_dict))
-    timer.start()
-    proc.wait()
-    timer.cancel()
-    print(status_dict)
+def execute_legion(instr):
+    legion_cmd = '(cd {}; {}/{} 0);\n'.format(LEGION_DIR, LEGION_CMD_PREFIX, instr)
 
+    with open(RESTRICTED_LEGION, 'w') as restrictor:
+        restrictor.writelines([RESTRICTION, legion_cmd, 'echo $?'])
+
+    # pdb.set_trace()
+    print(os.system('bash {}'.format(RESTRICTED_LEGION)))
+    # sp_command = ['bash', RESTRICTED_LEGION]
+    # print(subprocess.check_output(sp_command, cwd=LEGION_DIR))
+    # try:
+    #     print("Start running Legion")
+    #     # report = subprocess.check_output(sp_command, cwd=LEGION_DIR)
+    # except:
+    #     pass
+        # report = os.popen(command)
+        # print(report)
+    # pdb.set_trace()
+
+
+def execute_binary():
+    print("Compile source code for gcov...")
+    os.system("(cd gcov; {} {src} {src}.i ../c/__VERIFIER.c)".format(GCOV_CMD, src=src_name[:-2]))
+    print("Compute coverage...")
+    ordered_inputs = sorted([float(infile) for infile in os.listdir(
+        "{}/{}".format(INPUTS_DIR, src_name[:-2]))])
+    # pdb.set_trace()
+    print(len(ordered_inputs))
+    # pdb.set_trace()
+    for input_str in ordered_inputs:
+        print("    Execute binary with input...")
+        input_path = "{}/{}/{}".format(INPUTS_DIR, src_name[:-2], input_str)
+        command = "(cd gcov; ./{} < ../{})".format(src_name[:-2], input_path)
+        report = os.popen(command)
+        print(report.read())
+        # os.system("(cd gcov; ./{} < ../{})".format(src_name[:-2], input_path))
+        print("    Computing the coverage...")
+        percentage = os.popen("(cd gcov; gcov -b -c -a -u {} | grep 'Taken at least once:' | grep -o [0-9.]*%)".format(src_name[:-2])).read()
+        percentages.append(float(percentage[:-2]))
+
+
+def cleanup():
+    os.system("rm -rf gcov/*")
+    os.system("rm -rf c/instr/*")
+    os.system("rm -rf ../../Principes/inputs/*")
+
+
+cleanup()
 TARGET_DIRS = parse_target_dirs()
 # {line of source code : file name}
-length_dict = get_length_dict()
+unfiltered_length_dict = get_length_dict()
+blacklist = parse_blacklist()
+
+length_dict = {k: [file for file in v if file not in blacklist]
+               for k, v in unfiltered_length_dict.items()}
+
 sorted_lengths = list(sorted(length_dict.keys()))
 group_size = int(len(sorted_lengths)/NUM_NEEDS)
 grouped_lengths = \
@@ -130,30 +180,47 @@ lengths = [random.choice(group) for group in grouped_lengths]
 print(lengths)
 selected = [random.choice(list(length_dict[length])) for length in lengths]
 print(selected)
-
-# for code in selected:
-#     src_dir, src_name = code.split("/")
-#     os.system("(cd c; mkdir instr/{}; make {}instr)".format(
-#         src_dir, code[:-1]))
-#     if not os.listdir("c/instr/{}".format(src_dir)):
-#         os.system("rm -rf c/instr/{}".format(src_dir))
-#         continue
-#     os.system("cp c/{}c gcov/".format(src_name[:-1]))
-#     os.system("{} {} {}.c".format(GCOV_CMD, src_dir, code[:-1]))
-
-
-selected = ['array-examples/sanfoundry_24-1.instr']
-percentages = []
+selected = ['heap-manipulation/sll_to_dll_rev-11.i']
+all_percentages = []
 for code in selected:
     src_dir, src_name = code.split("/")
-    execute_legion(code)
-    os.system("(cd gcov; {} {src} {src}.c ../c/__VERIFIER.c)".format(GCOV_CMD, src=src_name[:-6]))
-    for input_str in os.listdir("{}/{}".format(INPUTS_DIR, src_name[:-6])):
-        input_path = "{}/{}/{}".format(INPUTS_DIR, src_name[:-6], input_str) 
-        os.system("(cd gcov; ./{} < ../{})".format(src_name[:-6], input_path))
-        percentage = os.popen("(cd gcov; gcov -b -c -a -u {} | grep 'Taken at least once:' | grep -o [0-9.]*\%)".format(src_name[:-6])).read()
-        percentages.append(float(percentage[:-2]))
+    print("Instrumenting source code...")
+    os.system("(cd c; mkdir instr/{}; make {}instr)".format(
+        src_dir, code[:-1]))
+    if not os.listdir("c/instr/{}".format(src_dir)):
+        print("Instrumentation unsuccessful, cleanup...")
+        os.system("rm -rf c/instr/{}".format(src_dir))
+        continue
+    print("Copy preprocessed code to gcov")
+    os.system("cp c/{}i gcov/".format(code[:-1]))
+    # os.system("{} {} {}.c".format(GCOV_CMD, src_dir, code[:-1]))
 
-print(percentages)
-pyplot.plot(percentages)
-pyplot.show()
+
+# selected = ['array-examples/sanfoundry_24-1.instr']
+    percentages = []
+    try:
+        print("Run Legion...")
+        execute_legion(code[:-1] + 'instr')
+    except:
+        pdb.set_trace()
+        pass
+
+    # execute_legion(code[:-1] + 'instr')
+
+    try:
+        execute_binary()
+        # print("Compile source code for gcov...")
+        # os.system("(cd gcov; {} {src} {src}.i ../c/__VERIFIER.c)".format(GCOV_CMD, src=src_name[:-2]))
+        # print("Compute coverage...")
+        # for input_str in os.listdir("{}/{}".format(INPUTS_DIR, src_name[:-2])):
+        #     print("    Execute binary with input...")
+        #     input_path = "{}/{}/{}".format(INPUTS_DIR, src_name[:-2], input_str)
+        #     os.system("(cd gcov; ./{} < ../{})".format(src_name[:-2], input_path))
+        #     print("    Computing the coverage...")
+        #     percentage = os.popen("(cd gcov; gcov -b -c -a -u {} | grep 'Taken at least once:' | grep -o [0-9.]*\%)".format(src_name[:-2])).read()
+        #     percentages.append(float(percentage[:-2]))
+    except:
+        pass    
+    all_percentages.append(percentages)
+
+print(all_percentages)
